6cb02dbba98aaac94e00c38d1beeca4e5f710823
Sam Hartharn-Evans <57717460+HartharnSam@users.noreply.github.com>
2025-07-03 14:32:18 +0100

    Create startup.m

---
+++ startup.m
@@ -0,0 +1,8 @@
+% Set some figure defaults
+set(0, 'DefaultAxesColorOrder', [0 0 0; 0 119 187; 0 153 136; ...
+   238 119 51; 204 51 17; 238 51 119; 187 187 187; 51 187 238]/255);
+% Color order from Vibrant of Paul Tol (personal.sron.nl/~pault/#sec:qualitative )
+
+set(0, 'DefaultSurfaceFaceColor', 'flat', 'DefaultSurfaceEdgeColor', 'none');
+set(0, 'DefaultFigureColormap', fake_parula(256));
+set(0, 'defaultLegendAutoUpdate','off')
